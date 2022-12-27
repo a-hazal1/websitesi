e677a92f5df5c5d7913d62c7c9d85b1dc368a58a
a-hazal1 <115560212+a-hazal1@users.noreply.github.com>
2022-12-27 13:12:43 +0300

    Log in işlemi yapıldı

--- home/models.py
+++ home/models.py
@@ -70,3 +70,5 @@ class ContactFormu(ModelForm):
 
 
 
+
+
--- home/views.py
+++ home/views.py
@@ -29,10 +29,9 @@ def iletisim(request):
             data.save()  # save data to table
             messages.success(request, "Your message has ben sent. Thank you for your message.")
             return HttpResponseRedirect('/iletisim')
-
     setting = Setting.objects.get(pk=1)
     form = ContactFormu()
-    context={'setting':setting}
+    context={'setting':setting, 'form':form}
     return render(request, 'iletisim.html', context)
 def fotograflarimiz(request):
     setting = Setting.objects.get(pk=1)
@@ -42,19 +41,15 @@ def blog(request):
     setting = Setting.objects.get(pk=1)
     context={'setting':setting}
     return render(request, 'blog.html', context)
-def uyegiris(request):
-    setting = Setting.objects.get(pk=1)
-    context={'setting':setting}
-    return render(request, 'uyegiris.html', context)
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect('/login')
 
 def login_view(request):
     if request.method == 'POST':  # check post
-            email = request.POST['email']
+            username = request.POST['username']
             password = request.POST['password']
-            user=authenticate(request, email=email,password=password)
+            user=authenticate(request, username=username ,password=password)
             if user is not None:
                 return HttpResponseRedirect('/')
             else:
@@ -73,5 +68,5 @@ def signup_view(request):
             login(request,user)
             return HttpResponseRedirect('/')
     form = SignUpForm()
-    context = {'form': form}
+    context={'form':form}
     return render(request, 'signup.html', context)
